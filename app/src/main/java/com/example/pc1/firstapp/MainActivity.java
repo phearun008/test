7ccc384d6bee3db10e52ebcddc0d092c8355cbbf
phearun008 <PC1@192.168.178.71>
2016-08-29 17:35:19 +0700

    first commit

---
+++ app/src/main/java/com/example/pc1/firstapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.pc1.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String url = "http://mobile-news.herokuapp.com/";
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+
+    }
+}
